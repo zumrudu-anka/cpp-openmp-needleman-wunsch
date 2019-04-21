3180c43592608361c661696caa90d69bc6f27004
zumrudu-anka <osmandurdag@hotmail.com>
2019-04-21 23:50:07 +0300

    Sonuc Bulma Algoritmasi iyilestirildi ve sonuclar hizali yazilmaya calisildi(Sonuclar buyukten kucuge siralanacak)

--- ParalelBilg.cpp
+++ ParalelBilg.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <fstream>
-
+#include <iomanip>
 using namespace std;
 
 int main()
@@ -11,83 +11,198 @@ int main()
     ifstream dosya1;
     dosya1.open("Sequences//5K_Sequence.fasta");
     int count=0;
-    string temp[15002];
-
+    
+    /*	Dosyanin tum satirlarini bellekte tutmak istersek bu sekilde gezinebiliriz... Goto TumSatirlarTag
+	string temp[15002];
+	
     while(!dosya1.eof()){
         getline(dosya1,temp[count]);
         count++;
     }
-
+	*/
+	
+	string temp[5000];
+	string templine;
+	//	ilk 3 satiri okumuyoruz...
+	getline(dosya1,templine);
+	getline(dosya1,templine);
+	getline(dosya1,templine);
+	
+	// 4. satirdan itibaren 2 satir bos gecip okuma yapiyoruz boylece bellekte kapladigimiz alani kucuk tutmaya calistik...
+	while(!dosya1.eof()){
+		getline(dosya1,temp[count]);
+		getline(dosya1,templine);
+		getline(dosya1,templine);
+		count++;
+	}
+	
     dosya1.close();
 
     string sekansx, sekansy;
 
-	double scores[20];
+	float scores[60];
+	float minvalue_of_scores; 
+	int minvalueindex_of_scores;
 	int index_of_scores=0;
-    for(int i=3; i<15001; i+=3){
+    /*
+    
+    Dosyadaki tum satirlari bellekte tutmak istersek bu sekilde gezinme yapabiliriz...
+	
+	TumSatirlarTag:
+	
+		for(int i=3; i<15001; i+=3){
+	        sekansx = temp[i];
+	        for(int j=i+3; j<14998; j+=3){
+	            sekansy = temp[j];
+	            float matrix[202][202];
+	            matris[0][0]=0;
+	            gap= -1.832482334;
+	            for(int k=1; k<202; k++){
+	                matrix[0][k]= gap;
+	                gap +=gap;
+	
+	            }
+	            gap= -1.832482334;
+	            for(int k=1; k<202; k++){
+	                matrix[k][0]= gap;
+	                gap +=gap;
+	            }
+	            gap= -1.832482334;
+	
+	            float x,y,z;
+	            int p=0;
+	            for(int k=1; k<202; k++){
+	                for(int t=1; t<202; t++){
+	                        y=gap+matrix[k][t-1];
+	                        z=gap+matrix[k-1][t];
+	
+	                    if(sekansx[k]==sekansy[t]){
+	                        x=match+matrix[k-1][t-1];
+	                    }
+	                    else{
+	                         x=missmatch+matrix[k-1][t-1];
+	                    }
+	                    matrix[k][t]=x>=z?(y>=x?y:x):(z>=y?z:y);
+	
+	                }
+	
+	            }
+	            if(index_of_scores < 20){
+					scores[index_of_scores] = matrix[201][201];
+	            	index_of_scores++;
+				}
+	            else{
+	        		double max = scores[0];
+	        		int tempindex = 0;
+	        		for(int k = 1; k < 20; k++){
+	        			if(scores[k] > max){
+							max = scores[k];
+							tempindex = k;
+						}
+					}
+					if(max > matrix[201][201]){
+						scores[tempindex] = matrix[201][201];
+					}
+					
+				}
+	            cout<<matrix[201][201]<<endl;
+	        }
+	    }
+	*/
+    
+    for(int i = 0; i < 4999; i++){
         sekansx = temp[i];
-
-        for(int j=i+3; j<14998; j+=3){
+        for(int j = i + 1; j < 5000; j++){
             sekansy = temp[j];
-            float matris [202][202];
-            matris[0][0]=0;
+            float matrix[202][202];
+            matrix[0][0] = 0;
             gap= -1.832482334;
-            for(int k=1; k<202; k++){
-                matris[0][k]= gap;
-                gap +=gap;
+            for(int k = 1; k < 202; k++){
+                matrix[0][k] = gap;
+                gap += gap;
 
             }
-            gap= -1.832482334;
-            for(int k=1; k<202; k++){
-                matris[k][0]= gap;
-                gap +=gap;
+            gap = -1.832482334;
+            for(int k = 1; k < 202; k++){
+                matrix[k][0] = gap;
+                gap += gap;
             }
-            gap= -1.832482334;
+            gap = -1.832482334;
 
-            float x,y,z;
-            int p=0;
-            for(int k=1; k<202; k++){
-                for(int t=1; t<202; t++){
-                        y=gap+matris[k][t-1];
-                        z=gap+matris[k-1][t];
+            float x, y, z;
+            for(int k = 1; k < 202; k++){
+                for(int t = 1; t < 202; t++){
+                        y = gap + matrix[k][t-1];
+                        z = gap + matrix[k-1][t];
 
-                    if(sekansx[k]==sekansy[t]){
-                        x=match+matris[k-1][t-1];
+                    if(sekansx[k] == sekansy[t]){
+                        x = match + matrix[k-1][t-1];
                     }
                     else{
-                         x=missmatch+matris[k-1][t-1];
+                         x = missmatch + matrix[k-1][t-1];
                     }
-                    matris[k][t]=x>=z?(y>=x?y:x):(z>=y?z:y);
+                    matrix[k][t] = x >= z ? (y >= x ? y : x) : (z >= y ? z : y);
 
                 }
 
             }
             if(index_of_scores < 20){
-				scores[index_of_scores] = matris[201][201];
-            	index_of_scores++;
-			}
-            else{
-        		double max = scores[0];
-        		int tempindex = 0;
-        		for(int k = 1; k < 20; k++){
-        			if(scores[k] > max){
-						max = scores[k];
-						tempindex = k;
+				
+				scores[index_of_scores * 3] = i;
+	            scores[index_of_scores * 3 + 1] = j;
+				scores[index_of_scores * 3 + 2] = matrix[201][201];
+            	
+				index_of_scores++;
+				
+				// ilk 20 skor degeri hesaplandigi an aralarinda maximum skor degerini belirliyoruz...
+				if(index_of_scores == 20){
+					minvalue_of_scores = scores[2];
+					minvalueindex_of_scores = 0;
+					for(int k = 1; k < 20; k++){
+						if(scores[lk * 3 + 2] > maxvalue_of_scores){
+							minvalue_of_scores = scores[k * 3 + 2];
+							minvalueindex_of_scores = k; 
+						}
 					}
-				}
-				if(max > matris[201][201]){
-					scores[tempindex] = matris[201][201];
 				}
 				
 			}
-            cout<<matris[201][201]<<endl;
+            else{
+            	
+            	if(minvalue_of_scores < matrix[201][201]){
+					
+					scores[minvalueindex_of_scores * 3] = i;
+					scores[minvalueindex_of_scores * 3 + 1] = j;
+					scores[minvalueindex_of_scores * 3 + 2] = matrix[201][201];
+				
+					minvalue_of_scores = scores[2];
+					int minvalueindex_of_scores = 0;
+					
+					for(int k = 1; k < 20; k++){
+						if(scores[k * 3 + 2] < minvalue_of_scores){
+							minvalue_of_scores = scores[k * 3 + 2];
+							minvalueindex_of_scores = k;
+						}
+					}
+				}				
+			}
+            cout<<matrix[201][201]<<endl;
         }
     }
     
     ofstream dosya2("sonuclar.txt");
-    for(int i=0;i<20;i++){
-    	dosya2<<scores[i]<<endl;
-	}
+    dosya2<<endl<<setw(70)<<"SKOR TABLOSU"<<endl<<endl<<endl;
+    dosya2<<setw(10)<<"No"<<setw(10)<<"S1"<<setw(10)<<"S2"<<setw(20)<<"Skor"<<endl<<endl;
+	for(int i=0;i<20;i++){
+		if(i==19){
+			dosya2<<setw(10)<<i<<setw(10)<<scores[i * 3]<<setw(10)<<scores[i * 3 ]<<setw(10)<<scores[i];
+		}
+		else{
+			dosya2<<setw(10)<<i<<setw(10)<<scores[i * 3]<<setw(10)<<scores[i * 3 ]<<setw(10)<<scores[i]<<endl;
+		}
+		
+    }
     dosya2.close();
+    
     return 0;
 }
